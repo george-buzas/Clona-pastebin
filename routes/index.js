e945230c05259b123276cd48411a105e51e3a982
George Buzas <g.buzas@student.rug.nl>
2021-12-22 20:13:13 +0200

    updated version of clone pastebin

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,11 @@ module.exports = router;
 
 function redirect() {
   /*
-    Between the quotes it should be a url, that redirects the user to another web page, that contains the text that has been pasted in the 
+    Between the quotes it should be a url, that redirects the user to another web page, which contains the text that has been pasted in the 
     initial text area.
     However, I am not sure if I should create another separate html page for this, my guess is that I would have to create one.
   */
-  document.location.href = '';
+    let txt = document.getElementById("text").value;
+    //window.location.href="D:/Web_Projects_WellCode/Clona_Pastebin/public/html/redirect.html";
+    document.write(txt);
 }
