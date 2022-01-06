87fc3c38a55985cc5d5492afa8342c7f67f138f4
George Buzas <g.buzas@student.rug.nl>
2022-01-06 21:36:40 +0200

    connection to db and CRUD operations

--- app.js
+++ app.js
@@ -1,41 +1,51 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//Importing packages
+const express = require('express');
+//Execute express on top of the app
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-var app = express();
+//Using  dotenv package
+require('dotenv/config');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(cors());
+app.use(bodyParser.json());
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+//Definig the port
+const PORT = 3000;
+
+//Import Routes
+const postsRoute = require('./routes/posts');
+app.use('/posts', postsRoute);
+
+
+//Routes
+// This executes when we go to: http://localhost:3000/
+app.get('/', (req, res) => {
+  res.send('We are on home');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION)
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log("Database connection is Ready "
+        + "and Server is Listening on Port ", PORT);
+    })
+})
+.catch((err)=>{
+    console.log("A error has been occurred while"
+        + " connecting to database.");   
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//Listening to the server
+//app.listen(PORT);
 
-module.exports = app;
+function redirect() {
+    let txt = document.getElementById("text").value;
+    document.write(txt);
+}
